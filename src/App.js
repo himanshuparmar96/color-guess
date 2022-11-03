ce3e62b2456c25afcb5f0037cf9827b24641bfc4
Himanshu Parmar <parmarhimanshu96@gmail.com>
2022-11-03 19:47:53 +0530

    Reset component and partial score done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Alert, Box, Button, Grid, Item, Snackbar } from "@mui/material";
+import _ from "lodash";
+import { useEffect, useState } from "react";
 
+function getRandomColor() {
+  const base = "0123456789abcdef";
+  let output = "";
+  for (let i = 0; i < 6; i++) {
+    const index = _.random(0, base.length - 1);
+    output += base[index];
+  }
+  return `#${output}`;
+}
+
+const TOTAL_BUTTONS = 5;
+// const reset = false;
 function App() {
+  const [color, setColor] = useState();
+  const [optionaColor, setoptionalcolor] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [severity, setSeverity] = useState("error");
+  const [score, setScore] = useState(0);
+  useEffect(() => {
+    const arr = [];
+    for (let i = 0; i < TOTAL_BUTTONS; i++) {
+      arr.push(getRandomColor());
+    }
+    setoptionalcolor(arr);
+    const ind = _.random(0, TOTAL_BUTTONS - 1);
+    setColor(arr[ind]);
+  }, [score]);
+
+  function validate(random) {
+    setOpen(false);
+    if (random === color) {
+      setSeverity("success");
+      setScore((score) => score + 1);
+    } else {
+      setSeverity("error");
+    }
+    setOpen(true);
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  // console.log(optionaColor);
   return (
-    <div className="App">
-      <h1>welcome back</h1>
-      <h2>hey there!</h2>
-      </div>
+    <div>
+      <h1>Score: {score}</h1>
+      <Box
+        sx={{
+          height: 300,
+          backgroundColor: color,
+        }}
+      />
+
+      <Box display={"flex"} justifyContent={"space-around"}>
+        {optionaColor.map((randomColor, index) => {
+          return (
+            <Button
+              variant="contained"
+              key={index}
+              onClick={() => validate(randomColor)}
+            >
+              {randomColor}
+            </Button>
+          );
+        })}
+      </Box>
+      <Snackbar open={open} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+          {severity === "success" ? "Correct!!" : "Incorrect guess!"}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 }
 
